ee8d68cda3b4db45098911b52640e4da4b941da0
vijay <vijaybalajitheboss@gmail.com>
2020-07-17 21:47:00 +0530

    Added in archive extensions

--- src/modules/extensions.py
+++ src/modules/extensions.py
@@ -7,16 +7,22 @@ list_documents = [
     ".doc",".txt",".pdf",".xlsx",".docx",".xls",".rtf",".md",".ods",".ppt",".pptx"
     ]
 list_videos = [
-    ".mp4",".mkv"
+    ".mp4",".m4v",".f4v",".f4a",".m4b",".m4r",".f4b",".mov",".3gp",
+    ".3gp2",".3g2",".3gpp",".3gpp2",".ogg",".oga",".ogv",".ogx",".wmv",
+    ".asf*",".webm",".flv",".avi",".QuickTime",".HDV",".OP1a",".OP-Atom",".MPEG-TS",".wav",".lxf",".gxf"
     ]
 list_audio = [
-    ".mp3",".wav",".m4a"
+    ".mp3",".wav",".m4a",".aac",".he-aac",".ac3",".eac3",".vorbis",".wma",".pcm"
     ]
 list_applications = [
     ".exe",".lnk"
     ]
 list_codes = [
     ".c",".py",".java",".cpp",".js",".html",".css",".php"
+    ]
+
+list_archives = [
+    ".zip",".7-zip",".7z",".bz2",".gz",".rar",".tar"
     ]
 
 extensions = {
@@ -26,4 +32,5 @@ extensions = {
     "Audio" : list_audio,
     "Applications" : list_applications,
     "Code" : list_codes,
+    "Archives" : list_archives
     }
