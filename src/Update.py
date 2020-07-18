dcfa03aebaa18872836f9736a02e78f2803ee9c7
vijay <vijaybalajitheboss@gmail.com>
2020-07-18 22:34:19 +0530

    Completely remade update layout and seperated functions

--- src/Update.py
+++ src/Update.py
@@ -12,5 +12,31 @@ if __name__ == "__main__":
     version = open("../assets/version.txt" , "r").read()
     cyan("File Sorter | " + version)
     while(True):
-        functions_main()
+        print("1) Add an extension to an already existing category")
+        print("2) Add an extension to a new catagory")
+        print("3) Delete an extension")
+        print("4) View extensions")
+        print("5) Quit")
+        choice = int(input("What would you like to do?\n> "))
+        
+        pickle_file = open("../assets/extensions_data.pkl", 'rb')
+        extensions = pickle.load(pickle_file)
+        pickle_file.close()
+
+        if(choice == 1):
+            add_data(extensions)
+        elif(choice == 2):
+            add_category(extensions)
+        elif(choice == 3):
+            delete_data(extensions)
+        elif(choice == 4):
+            view_data(extensions)
+        elif(choice == 5):
+            green("---- x Thanks for using File Sorter x ----")
+            grey("Press enter to exit...")
+            input()
+            break
+        else:
+            red("\nERROR : INVALID OPTION")
+
         green("\n"+"-"*20)
